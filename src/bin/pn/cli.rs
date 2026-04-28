186b259c2b86e486f550b1d822200eb4b3225d5b
Jonathon Jongsma <jonathon@quotidian.org>
2026-04-28 09:16:46 -0500

    Add a new commandline application 'pn'
    This will be the cli UI for the project
    
    Signed-off-by: Jonathon Jongsma <jonathon@quotidian.org>

---
+++ src/bin/pn/cli.rs
@@ -0,0 +1,20 @@
+#[derive(Debug, clap::Parser)]
+pub struct Options {
+    #[command(subcommand)]
+    pub command: MainCommand,
+}
+
+#[derive(Debug, clap::Subcommand)]
+pub enum MainCommand {
+    #[command(about = "Taxonomy-related commands")]
+    Taxon {
+        #[command(subcommand)]
+        command: TaxonCommands,
+    },
+}
+
+#[derive(Debug, clap::Subcommand)]
+pub enum TaxonCommands {
+    #[command(about = "Search for a taxon")]
+    Search { search_string: String },
+}
---
+++ src/bin/pn/main.rs
@@ -0,0 +1,117 @@
+use clap::Parser;
+use propagation_notebook::taxonomy::{Synonym, Taxon, VernacularName};
+use toasty::Db;
+
+use crate::cli::Options;
+
+mod cli;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    tracing_subscriber::fmt::init();
+    let options = Options::parse();
+    let mut db = Db::builder()
+        .models(propagation_notebook::models())
+        .connect("sqlite:./propagation-notebook.sqlite")
+        .await?;
+    match options.command {
+        cli::MainCommand::Taxon { command } => match command {
+            cli::TaxonCommands::Search { search_string } => {
+                tracing::debug!("Searching for exact complete name");
+                if let Ok(found) = Taxon::filter(Taxon::fields().complete_name().eq(&search_string))
+                    .one()
+                    .exec(&mut db)
+                    .await
+                {
+                    println!("found taxon {}: '{}'", found.id, found.complete_name);
+                } else {
+                    tracing::debug!("Searching for approximate complete name");
+                    let wildcard = format!("%{search_string}%");
+                    let taxa = Taxon::filter(Taxon::fields().complete_name().like(&wildcard))
+                        .exec(&mut db)
+                        .await?;
+                    if !taxa.is_empty() {
+                        println!("Possible options for '{search_string}':");
+                        for t in taxa {
+                            println!("- {}: {}", t.id, t.complete_name);
+                        }
+                    } else {
+                        tracing::debug!("Searching for exact scientific synonym");
+                        if let Ok(found) =
+                            Synonym::filter(Synonym::fields().complete_name().eq(&search_string))
+                                .include(Synonym::fields().taxon())
+                                .one()
+                                .exec(&mut db)
+                                .await
+                        {
+                            println!(
+                                "Found '{}' which is a synonym for {}: '{}'",
+                                found.complete_name,
+                                found.taxon.get().id,
+                                found.taxon.get().complete_name
+                            );
+                        } else {
+                            tracing::debug!("Searching for approximate scientific synonyms");
+                            let synonyms =
+                                Synonym::filter(Synonym::fields().complete_name().like(&wildcard))
+                                    .include(Synonym::fields().taxon())
+                                    .exec(&mut db)
+                                    .await?;
+                            if !synonyms.is_empty() {
+                                println!("Possible options for '{search_string}':");
+                                for syn in synonyms {
+                                    println!(
+                                        "'{}' is a synonym for {}: '{}'",
+                                        syn.complete_name,
+                                        syn.taxon.get().id,
+                                        syn.taxon.get().complete_name
+                                    );
+                                }
+                            } else {
+                                tracing::debug!("Searching for exact vernacular name");
+                                // look up common names
+                                if let Ok(vernacular) = VernacularName::filter(
+                                    VernacularName::fields().name().eq(&search_string),
+                                )
+                                .include(VernacularName::fields().taxon())
+                                .one()
+                                .exec(&mut db)
+                                .await
+                                {
+                                    println!(
+                                        "Found {}: '{} ({})'",
+                                        vernacular.taxon.get().id,
+                                        vernacular.taxon.get().complete_name,
+                                        vernacular.name
+                                    );
+                                } else {
+                                    tracing::debug!("Searching for approximate vernacular names");
+                                    let vernaculars = VernacularName::filter(
+                                        VernacularName::fields().name().like(&wildcard),
+                                    )
+                                    .include(VernacularName::fields().taxon())
+                                    .exec(&mut db)
+                                    .await?;
+                                    if !vernaculars.is_empty() {
+                                        println!("Possible options for '{search_string}':");
+                                        for vernacular in vernaculars {
+                                            println!(
+                                                "{}: '{} ({})'",
+                                                vernacular.taxon.get().id,
+                                                vernacular.taxon.get().complete_name,
+                                                vernacular.name,
+                                            );
+                                        }
+                                    } else {
+                                        println!("No options found");
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        },
+    }
+    Ok(())
+}
