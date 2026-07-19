386554061fbbfde2ed7c5c165881ccd3dafb040c
Jonathon Jongsma <jonathon@quotidian.org>
2026-07-19 12:11:20 -0500

    libpropagation: avoid constructing default db uri if one is specified
    Only construct the default db uri if the PN_DB_URI environment variable is not set.
    
    Signed-off-by: Jonathon Jongsma <jonathon@quotidian.org>

--- libpropagation/src/lib.rs
+++ libpropagation/src/lib.rs
@@ -16,20 +16,22 @@ pub fn models() -> ModelSet {
 }
 
 pub async fn db() -> Result<toasty::Db, Error> {
-    let project_dir = ProjectDirs::from("org", "quotidian", "propagation-notebook")
-        .ok_or_else(|| Error::Runtime(error::Runtime::ProjectDirNotFound))?
-        .data_dir()
-        .to_path_buf();
-    std::fs::create_dir_all(&project_dir)?;
     let db_uri = match std::env::var("PN_DB_URI") {
         Ok(s) => Ok(s),
-        Err(std::env::VarError::NotPresent) => Ok(format!(
-            "sqlite:{}",
-            project_dir
-                .join("propagation-notebook.sqlite")
-                .to_str()
-                .unwrap()
-        )),
+        Err(std::env::VarError::NotPresent) => {
+            let project_dir = ProjectDirs::from("org", "quotidian", "propagation-notebook")
+                .ok_or_else(|| Error::Runtime(error::Runtime::ProjectDirNotFound))?
+                .data_dir()
+                .to_path_buf();
+            std::fs::create_dir_all(&project_dir)?;
+            Ok(format!(
+                "sqlite:{}",
+                project_dir
+                    .join("propagation-notebook.sqlite")
+                    .to_str()
+                    .unwrap()
+            ))
+        }
         Err(e) => Err(Error::Runtime(error::Runtime::InvalidEnvVar(e.to_string()))),
     }?;
     Ok(toasty::Db::builder()
