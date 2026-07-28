16a7d7cbe3e606d9f0438cf7a848cbead667b8fb
Jonathon Jongsma <jonathon@quotidian.org>
2026-07-27 21:18:32 -0500

    web: add ability to show regional taxa ready-to-harvest
    Signed-off-by: Jonathon Jongsma <jonathon@quotidian.org>

--- web/src/regions.rs
+++ web/src/regions.rs
@@ -1,18 +1,19 @@
 use libpropagation::{
-    region::{Region, RegionalTaxonStatus},
+    region::{Origin, Region, RegionalTaxonStatus},
     taxonomy::Taxon,
 };
+use serde::Serialize;
 use topcoat::{
     context::Cx,
     router::{page, path_param, query_params},
-    view::view,
+    view::{attributes, view},
 };
 use tracing::trace;
 
 use crate::{
     components::pagination_control,
     leaflet::Map,
-    util::{PageQueryParams, PageState, Path, RegionId, TaxonId, db},
+    util::{ModifyOffset, PageState, Path, RegionId, TaxonId, db},
 };
 
 #[page("/regions")]
@@ -69,20 +70,67 @@ pub(crate) async fn details(cx: &Cx) -> topcoat::Result {
     }
 }
 
+#[derive(Debug, Clone, Serialize)]
+#[query_params(error = bad_request)]
+pub struct RegionalTaxaListParams {
+    pub offset: Option<usize>,
+    pub ready: Option<bool>,
+}
+
+impl ModifyOffset for RegionalTaxaListParams {
+    fn modify_offset(&mut self, new_offset: usize) {
+        self.offset = Some(new_offset)
+    }
+}
+
 #[page("/regions/{region_id}/taxa")]
 pub async fn taxa_list(cx: &Cx) -> topcoat::Result {
     let mut db = db(cx);
     let region_id = path_param::<RegionId>(cx)?;
-    let params = query_params::<PageQueryParams>(cx)?;
-    let filter = Taxon::filter(
-        Taxon::fields()
-            .regional_statuses()
-            .any(RegionalTaxonStatus::fields().region_id().eq(region_id)),
-    );
+    let params = query_params::<RegionalTaxaListParams>(cx)?;
+    let mut rts_filter = RegionalTaxonStatus::fields().region_id().eq(region_id);
+    if params.ready == Some(true) {
+        let day = jiff::Zoned::now().date().day_of_year();
+        // include species that start harvesting in the next week
+        let start = day;
+        // include species that finished harvesting a week ago
+        let end = day;
+        rts_filter = rts_filter.and(
+            RegionalTaxonStatus::fields()
+                .harvest_window()
+                .start_doy()
+                .le(start)
+                .and(
+                    RegionalTaxonStatus::fields()
+                        .harvest_window()
+                        .end_doy()
+                        .ge(end),
+                )
+                .or(RegionalTaxonStatus::fields()
+                    .harvest_window()
+                    .start_doy()
+                    .gt(RegionalTaxonStatus::fields().harvest_window().end_doy())
+                    .and(
+                        RegionalTaxonStatus::fields()
+                            .harvest_window()
+                            .start_doy()
+                            .le(start)
+                            .or(RegionalTaxonStatus::fields()
+                                .harvest_window()
+                                .end_doy()
+                                .ge(end)),
+                    )),
+        );
+    }
+    let filter = Taxon::filter(Taxon::fields().regional_statuses().any(rts_filter));
     let total = filter.clone().count().exec(&mut db).await? as usize;
     let page_state = PageState::new(params.offset, total);
     let taxa = filter
-        .include(Taxon::fields().regional_statuses())
+        .include(
+            Taxon::fields()
+                .regional_statuses()
+                .filter(RegionalTaxonStatus::fields().region_id().eq(region_id)),
+        )
         .order_by(Taxon::fields().sequence().asc())
         .limit(page_state.per_page)
         .offset(page_state.offset)
@@ -91,19 +139,34 @@ pub async fn taxa_list(cx: &Cx) -> topcoat::Result {
     let region = Region::get_by_id(&mut db, region_id).await?;
     view! {
         <h1>(&region.name)</h1>
-        <ul>
+        <table>
+            <tr>
+                <th>"Taxon"</th>
+                <th>"Origin"</th>
+                <th>"Harvest Dates"</th>
+            </tr>
             for taxon in taxa {
-                for rts in taxon.regional_statuses.get() {
-                    if rts.region_id == region.id {
-                        <li>
+                if let Some(rts) = taxon.regional_statuses.get().first() {
+                    <tr>
+                        <td>
                             <span class="latin">
                                 <a href=(rts.path())>(&taxon.complete_name)</a>
                             </span>
-                        </li>
-                    }
+                        </td>
+                        <td>
+                        if let Some(origin) = rts.origin {
+                            let attrs =  attributes!{
+                                match origin {Origin::Introduced  => class="introduced",
+                                    Origin::Native => class="native",
+                                    _ => class="" }
+                            };
+                            <span (attrs)>(origin.to_string())</span>
+                        }</td>
+                        <td>(rts.harvest_window.to_string())</td>
+                    </tr>
                 }
             }
-        </ul>
+        </table>
         pagination_control(state: &page_state, params: params)
     }
 }
