d8264bc8b01bf23af0709adaf9b021edc22284d5
Jonathon Jongsma <jonathon@quotidian.org>
2026-07-30 16:13:19 -0500

    web: add home page
    Signed-off-by: Jonathon Jongsma <jonathon@quotidian.org>

--- web/src/main.rs
+++ web/src/main.rs
@@ -135,5 +135,90 @@ async fn layout(slot: topcoat::Result) -> topcoat::Result {
 
 #[page("/")]
 async fn home() -> topcoat::Result {
-    view! { <h1>"Home"</h1> }
+    view! {
+        <h1>"Home"</h1>
+        <div>
+            <div class="my-4">
+                "To get started, look up information about a particular taxon: "
+            </div>
+            <form method="get" action="/taxa" class="flex my-4">
+                <input
+                    type="text"
+                    name="q"
+                    placeholder="Search for a taxon"
+                    class="me-2 flex-grow"
+                >
+                <button type="submit">"Search"</button>
+            </form>
+        </div>
+        <h2>"About This Site"</h2>
+        <div id="content" class="flex flex-col gap-4 text-lg">
+            <div>
+                <h3>"The Problem"</h3>
+                <div class="my-4">
+                    r#"There is not currently any single site that serves as a
+                    reference for how to propagate a particular species.  Native
+                    species tend to be more difficult to propagate and some
+                    species are still poorly understood. There is currently
+                    no single reference to consult when you want to know how to
+                    propagate a specific species. For commercially-available
+                    species, many people use the information provided by
+                    the seed producer. For species that are not available
+                    commercially, you generally have to search for studies
+                    or species-specific references. For more obscure or
+                    tricky-to-propagate species, ecological restoration
+                    professionals sometimes pass tips and tricks by word of
+                    mouth. "#
+                </div>
+            </div>
+            <div>
+                <h3>"The Goal"</h3>
+                <div class="my-4">
+                    r#"That's where this website comes in. The goal of this
+                    site is to become a comprehensive reference to collecting
+                    seeds and propagating native plants  for use in ecological
+                    restoration. In its current state, this site is fairly
+                    limited, but it is beginning to be useful. "#
+                </div>
+            </div>
+            <div>
+                <h3>"Features"</h3>
+                <div class="my-4">
+                    r#"There are a couple main focuses of this site. The first
+                    is propagation protocols. Because many species have very
+                    similar propagation protocols (e.g. "30 days of cold moist
+                    stratification"), these are modeled as general protocols that
+                    can be assigned to particular species. We hope to eventually
+                    collect propagation instructions for as many species as
+                    possible. This part of the site is intended for use by both
+                    home gardeners and ecological professionals.  "#
+                </div>
+                <div class="my-4">
+                    r#"The second main focus is seed collection. In order
+                    to restore habitat, you first need the propagation material.
+                    In many cases the best and easiest choice is seed. But
+                    realiable information about collecting, processing and
+                    storing the seeds is dispersed and often hard to find. This
+                    is especially true for species that are not available in the
+                    commercial trade. So our goal is to be a comprehensive
+                    reference for both how to collect the seed, as well as
+                    *when* to collect the seed."#
+                </div>
+                <div class="my-4">
+                    r#"Of course, the seed collection window is highly dependent
+                    on your geographical location, so there is a concept of
+                    "regions". Each region has a checklist of species and
+                    information about the status of that species within the
+                    region. One of the most relevant pieces of information is the
+                    dates that the species produces fruit or seeds within that
+                    region. At the moment, this site has a fairly limited set
+                    of regions in the upper midwest. In the future, we hope
+                    to expand this to many other regions and allow community
+                    management of this information. For now, it is centrally
+                    managed until I can get the infrastructure in place to
+                    support external contributions."#
+                </div>
+            </div>
+        </div>
+    }
 }
