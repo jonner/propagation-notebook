25c7f4d1ed2d14b554307bdeca2cafa016a52afb
Jonathon Jongsma <jonathon@quotidian.org>
2026-09-01 09:50:17 -0500

    web: change script to generate smaller images
    Signed-off-by: Jonathon Jongsma <jonathon@quotidian.org>

--- web/assets/convert_webp.sh
+++ web/assets/convert_webp.sh
@@ -3,6 +3,6 @@
 input="$1"
 output="${input%.*}.webp"
 
-magick "$input" -resize 1200x1200\> -quality 75 "$output"
+magick "$input" -resize 1200x1200 -strip -define webp:method=6 -define webp:alpha-compression=1 -define webp:auto-filter=true -quality 65 "$output"
 echo "Created WebP: $output"
 
