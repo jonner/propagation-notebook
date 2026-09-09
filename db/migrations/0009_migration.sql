e2f06cd758afa0d51e9f36b8de9c66b30ca6c1f6
Jonathon Jongsma <jonathon@quotidian.org>
2026-09-09 12:23:02 -0500

    db: Update auth migration
    This hasn't been applied yet, so just update it rather than creating a new one
    
    Signed-off-by: Jonathon Jongsma <jonathon@quotidian.org>

--- db/migrations/0009_migration.sql
+++ db/migrations/0009_migration.sql
@@ -1,8 +1,31 @@
+CREATE TABLE "roles" (
+    "id" BLOB NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TEXT NOT NULL,
+    "updated_at" TEXT NOT NULL,
+    PRIMARY KEY ("id")
+);
+-- #[toasty::breakpoint]
+CREATE INDEX "index_roles_by_name" ON "roles" ("name");
+-- #[toasty::breakpoint]
+CREATE TABLE "role_permissions" (
+    "role_id" BLOB NOT NULL,
+    "permission_id" BLOB NOT NULL,
+    "created_at" TEXT NOT NULL,
+    PRIMARY KEY ("role_id", "permission_id")
+);
+-- #[toasty::breakpoint]
+CREATE INDEX "index_role_permissions_by_role_id" ON "role_permissions" ("role_id");
+-- #[toasty::breakpoint]
+CREATE INDEX "index_role_permissions_by_permission_id" ON "role_permissions" ("permission_id");
+-- #[toasty::breakpoint]
 CREATE TABLE "sessions" (
     "id" BLOB NOT NULL,
     "token_hash" BLOB NOT NULL,
     "user_id" BLOB NOT NULL,
     "expires_at" TEXT NOT NULL,
+    "created_at" TEXT NOT NULL,
+    "updated_at" TEXT NOT NULL,
     PRIMARY KEY ("id")
 );
 -- #[toasty::breakpoint]
@@ -14,6 +37,9 @@ CREATE TABLE "users" (
     "id" BLOB NOT NULL,
     "username" TEXT NOT NULL,
     "pwhash" TEXT NOT NULL,
+    "created_at" TEXT NOT NULL,
+    "updated_at" TEXT NOT NULL,
+    "last_login_at" TEXT,
     PRIMARY KEY ("id")
 );
 -- #[toasty::breakpoint]
@@ -21,39 +47,24 @@ CREATE UNIQUE INDEX "index_users_by_username" ON "users" ("username");
 -- #[toasty::breakpoint]
 CREATE INDEX "index_users_by_pwhash" ON "users" ("pwhash");
 -- #[toasty::breakpoint]
-CREATE TABLE "roles" (
+CREATE TABLE "permissions" (
     "id" BLOB NOT NULL,
-    "name" TEXT NOT NULL,
+    "code" TEXT NOT NULL CHECK ("code" IN ('taxon:sync')),
+    "description" TEXT NOT NULL,
+    "created_at" TEXT NOT NULL,
+    "updated_at" TEXT NOT NULL,
     PRIMARY KEY ("id")
 );
 -- #[toasty::breakpoint]
-CREATE INDEX "index_roles_by_name" ON "roles" ("name");
+CREATE UNIQUE INDEX "index_permissions_by_code" ON "permissions" ("code");
 -- #[toasty::breakpoint]
 CREATE TABLE "user_roles" (
     "user_id" BLOB NOT NULL,
     "role_id" BLOB NOT NULL,
+    "created_at" TEXT NOT NULL,
     PRIMARY KEY ("user_id", "role_id")
 );
 -- #[toasty::breakpoint]
 CREATE INDEX "index_user_roles_by_user_id" ON "user_roles" ("user_id");
 -- #[toasty::breakpoint]
 CREATE INDEX "index_user_roles_by_role_id" ON "user_roles" ("role_id");
--- #[toasty::breakpoint]
-CREATE TABLE "permissions" (
-    "id" BLOB NOT NULL,
-    "code" TEXT NOT NULL CHECK ("code" IN ('taxon:sync')),
-    "description" TEXT NOT NULL,
-    PRIMARY KEY ("id")
-);
--- #[toasty::breakpoint]
-CREATE UNIQUE INDEX "index_permissions_by_code" ON "permissions" ("code");
--- #[toasty::breakpoint]
-CREATE TABLE "role_permissions" (
-    "role_id" BLOB NOT NULL,
-    "permission_id" BLOB NOT NULL,
-    PRIMARY KEY ("role_id", "permission_id")
-);
--- #[toasty::breakpoint]
-CREATE INDEX "index_role_permissions_by_role_id" ON "role_permissions" ("role_id");
--- #[toasty::breakpoint]
-CREATE INDEX "index_role_permissions_by_permission_id" ON "role_permissions" ("permission_id");
