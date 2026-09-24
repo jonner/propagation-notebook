258994339281f87cecde08392caab60d598a9153
Jonathon Jongsma <jonathon@quotidian.org>
2026-09-24 16:55:27 -0500

    WIP: update to topcoat 0.9
    Signed-off-by: Jonathon Jongsma <jonathon@quotidian.org>

--- web/src/error.rs
+++ web/src/error.rs
@@ -1,7 +1,7 @@
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
-    #[error(transparent)]
-    Topcoat(#[from] topcoat::Error),
+    #[error("{0}")]
+    Topcoat(topcoat::Error),
     #[error(transparent)]
     Io(#[from] std::io::Error),
     #[error(transparent)]
